c5e5fb07f3c4799af15e291bb21b262dc268a7a3
Ronak Ameta <ronakameta95@gmail.com>
2026-07-10 00:42:02 +0530

    UNDER PROCESSING

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let book=document.querySelector(".book-now");
-let booked=document.querySelector(".booked");
+let container=document.querySelector(".container");
 let cross=document.querySelector(".cross")
 
+
+
 book.addEventListener("click",()=>{
-    booked.style.display="block";    
+    container.style.display="block";
+    document.body.classList.add("lock");
+    
 });
 
 cross.addEventListener("click",()=>{
-    booked.style.display="none";    
+    container.style.display="none";  
+    document.body.classList.remove("lock");  
 });
 
 
@@ -33,25 +38,28 @@ burger.addEventListener("click", () => {
 });
 
 
-// let cart=document.querySelectorAll(".cart")
-// let cartbox=document.querySelector(".cart-box")
-// let order=document.querySelector(".orders")
+let cart=document.querySelectorAll(".cart")
+let cartbox=document.querySelector(".cart-box")
+let order=document.querySelector(".orders")
+let food=document.querySelectorAll(".food-name")
+let price=document.querySelectorAll(".price")
 
-// cart.forEach(e => {
-//     e.addEventListener("click",()=>{
-//         cartbox.style.display="block"; 
-        
-//     });
-// });
+cart.forEach((e,i) => {
+    e.addEventListener("click",()=>{
+        cartbox.style.display="block";
+        let li=document.createElement("li");
+        li.textContent=food[i].textContent +"  "+ price[i].textContent ;
+        console.log(li)
+        order.appendChild(li); 
+    });
+});
 
-// let cart=document.querySelector(".cart")
-// let cart_box=document.querySelector(".cart-box")
+let cross1=document.querySelector(".cross1")
 
-// cart.addEventListener("click",()=>{
-//            cart.classList("cart_box");
-//            alert("hi")
-//        });
-
+cross1.addEventListener("click",()=>{
+    cartbox.style.display="none";  
+      
+});
 
 
 
